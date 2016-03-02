c9d00dd851d5ea50cfcac448fa513ded51d75b22
Armand-B <armand.briquet@gmail.com>
2016-03-03 00:48:45 +0100

    Ajout voiture a la scene avec controle
    maintenant on a une voiture qui roule et qui peut faire bouger la balle,
    et j'ai ajouter un bouton pour le futur menu

---
+++ Project42/Assets/script camera/camera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+    public GameObject carempty;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = carempty.transform.position + offset;
+	
+	}
+}
