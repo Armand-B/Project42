4a7f7904836e796810d6ec040e066886d74917a4
Armand-B <armand.briquet@gmail.com>
2016-03-03 23:25:49 +0100

    Couleurs
    j'ai mis des couleurs

---
+++ Project42/Assets/reset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reset : MonoBehaviour {
+    public GameObject carempty;
+
+
+    void Update () {
+        if (Input.GetButton("r"))
+        {
+            carempty.transform.position = Vector3.zero;
+                }
+	
+	}
+}
